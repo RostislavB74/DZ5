eb780e12c548c99551524ca987bc612962868af5
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-09-23 13:24:43 +0300

    edit

--- main_currency.py
+++ main_currency.py
@@ -10,21 +10,30 @@ BASE_DIR = pathlib.Path()
 
 async def main(*argv):
 
+    if len(sys.argv) > 2:
+        cur_1 = sys.argv[2]
+    if len(sys.argv) > 3:
+        cur_2 = sys.argv[3]
+    else:
+        cur_1 = "EUR"
+        cur_2 = "USD"
     request = await main_request(sys.argv[1])
     if request:
         with open(BASE_DIR.joinpath('data.json'), 'r', encoding='utf-8') as f:
             d = json.load(f)
             with open(BASE_DIR.joinpath('cur_list.json'), 'r', encoding='utf-8') as f_cur_list:
                 d_cur_list = json.load(f_cur_list)
-                cur_1 = sys.argv[2]
-                cur_2 = sys.argv[3]
+                # cur_1 = sys.argv[2]
+                # cur_2 = sys.argv[3]
                 if cur_1 and cur_2 in d_cur_list:
                     # currency_set = set()
                     result = []
+
                     for elem in d:
                         for key, value in elem.items():
                             if key == 'date':
                                 dt = value
+                                res_dict = {dt: {}}
                                 continue
                         dt_1 = elem.get('exchangeRate')
                         for elem in dt_1:
@@ -33,7 +42,7 @@ async def main(*argv):
                             if elem.get('currency') == cur_1:
                                 res_eur = {cur_1: {'sale': elem.get(
                                     'saleRate'), 'purchase': elem.get('purchaseRate')}}
-                                res_dict = {dt: res_eur}
+                                res_dict[dt] = res_eur
                                 continue
                             if elem.get('currency') == cur_2:
                                 res_usd = {cur_2: {'sale': elem.get(
---
+++ parser.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser('2')
+    return parser
+
+
+if __name__ == "__main__":
+    print(main())
